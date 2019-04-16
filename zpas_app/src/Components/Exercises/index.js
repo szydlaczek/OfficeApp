feb818ce5ece3e9c25661b742d8d4e34078a7a6e
Grzegorz Szydło <szydlo.grzegorz@outlook.com>
2019-04-16 23:28:02 +0200

    LeftPane and RightPane

--- zpas_app/src/Components/Exercises/index.js
+++ zpas_app/src/Components/Exercises/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import {Grid, Paper} from '@material-ui/core';
+import LeftPane from './LeftPane';
+import RightPane from './RightPane';
+const styles = {
+    Paper: {
+        padding: 20, marginTop: 10, marginBottom: 10 
+    }
+}
+
 export default props  =>
 <Grid container sm={12}>
     <Grid item sm>
-        <Paper>
-            Left pane
-        </Paper>
+        <LeftPane styles={styles}/>
     </Grid>
     <Grid item sm>
-        <Paper>
-            Right pane
-        </Paper>
+        <RightPane styles={styles}/>
     </Grid>
 </Grid>
