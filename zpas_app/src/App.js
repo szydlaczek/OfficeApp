b9370295923465d779756a1260952a4b59d681ee
Grzegorz Szydlo <szydlak@gmail.com>
2019-04-18 13:54:14 +0200

    Add layout

--- zpas_app/src/App.js
+++ zpas_app/src/App.js
@@ -1,35 +1,16 @@
 import React, { Component, Fragment} from 'react';
-import Exercises from './Components/Exercises/index'
 
-import {Header, Footer} from './Components/Layouts/index';
 import './App.css';
-import {muscles, exercises} from './customStore';
+import AppLayout  from './Components/Layouts/AppLayout'
 
-class App extends Component {
-  state = {
-    exercises  
-  }
-
-  getExercisesByMuscles() {
-    return  this.state.exercises.reduce((exercises,exercise)=> {
-      const {muscles} =exercise
-      console.log(exercises);
-      console.log("1");
-      exercises[muscles] = exercises[muscles]
-      ? [...exercises[muscles], exercise]
-      : [exercise]
-      
-      return exercises
-    }, {})    
-  }
+class App extends Component { 
+  
 
   render() {
-    console.log(this.getExercisesByMuscles());
+    
     return (
-      <Fragment>
-          <Header/>
-          <Exercises/>
-          <Footer muscles ={muscles}/>
+      <Fragment >
+                 <AppLayout></AppLayout>
       </Fragment>
     );
   }
